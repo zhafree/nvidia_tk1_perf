ec21e09dd29e7e58dd898659e99df39f8951f1fc
Ludwig Schreier <ludwig.schreier@gmail.com>
2016-10-27 23:04:58 +0200

    introduced options for 'fan' and cpus 1-3 'off' -- test pending

--- tegra_max_perf.sh
+++ tegra_max_perf.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 #
-# Turn on all performance for the TK1
+# Turn on / off performance Tegra TK1
+#
+# !!DISCLAIMER!!
+#
+# THE SCRIPT CHANGES CPU, GPU AND THEIR FREQUENIES,
+# LEADING TO POSSIBLE HEATING OR ELECTRICAL ISSUES.
+# MAKE SURE YOUR CHIP HAS ENOUGH COOLING.
+# USE AT YOUR OWN RISK!
 #
 # Version control system: See Git
 #
-# Anti USB freeze
+# Run as ./tegra_max_perf.sh <option1> <option2>
+#
+# Options:
+#
+# fan -> enable fan if possible
+# off -> put CPUs 1-3 (0,1,2,3) offline
+#
 
-echo -1 > /sys/module/usbcore/parameters/autosuspend
 
-# turn on fan for safety
-echo "Enabling fan for safety..."
-if [ ! -w /sys/kernel/debug/tegra_fan/target_pwm ] ; then
-	echo "Cannot set fan -- exiting..."
-fi
-echo 255 > /sys/kernel/debug/tegra_fan/target_pwm
+# Turn on cooling fan for safety
+if [ "$1" = "fan" ]
+then
+
+	echo "Enabling fan for safety."
+	if [ ! -w /sys/kernel/debug/tegra_fan/target_pwm ] ; then
+	echo "Cannot set fan -- exiting"
+	fi
+	echo 255 > /sys/kernel/debug/tegra_fan/target_pwm
+
+else
 
 echo 0 > /sys/devices/system/cpu/cpuquiet/tegra_cpuquiet/enable
 echo 1 > /sys/kernel/cluster/immediate
@@ -21,14 +38,27 @@ echo 1 > /sys/kernel/cluster/force
 echo G > /sys/kernel/cluster/active
 echo "Cluster: `cat /sys/kernel/cluster/active`"
 
-# online all CPUs - ignore errors for already-online units
-echo "onlining CPUs: ignore errors..."
-for i in 0 1 2 3 ; do
-	echo 1 > /sys/devices/system/cpu/cpu${i}/online
-done
-echo "Online CPUs: `cat /sys/devices/system/cpu/online`"
+if [ "$2" = "off" ]
+
+	# offline CPU 1-3
+	echo "Offlining CPUs: ignore errors..."
+	for i in 1 2 3 ; do
+		echo 0 > /sys/devices/system/cpu/cpu${i}/online
+	done
+	echo "Offline CPUs: `cat /sys/devices/system/cpu/online`"
+
+else
+
+	# online all CPUs - ignore errors for already-online units
+	echo "Onlining CPUs: ignore errors..."
+	for i in 0 1 2 3 ; do
+		echo 1 > /sys/devices/system/cpu/cpu${i}/online
+	done
+	echo "Online CPUs: `cat /sys/devices/system/cpu/online`"
+
 
 # set CPUs to max freq (perf governor not enabled on L4T yet)
+
 echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 cpumax=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $NF}'`
 echo "${cpumax}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
@@ -46,3 +76,6 @@ cat /sys/kernel/debug/clock/emc/max > /sys/kernel/debug/clock/override.emc/rate
 echo 1 > /sys/kernel/debug/clock/override.emc/state
 echo "EMC: `cat /sys/kernel/debug/clock/emc/rate`"
 
+
+	fi
+fi
